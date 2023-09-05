5d0fb2dc1c285414244acc8948b188be25993a8d
minhang <wu.minhang@outlook.com>
2023-09-05 19:41:41 +0800

    Finished tic-tac-toe using React

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,7 +1,64 @@
+import { useState } from "react";
 import "./App.css";
+import Board from "./Components/Board";
 
 function App() {
-  return <div className="App"></div>;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  let status = "";
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Turn: " + (xIsNext ? "X" : "O");
+  }
+
+  const handlePlay = (newSquares) => {
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const restartGame = () => {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="game-title">Tic-Tac-Toe</h1>
+      <h2 className="status">{status}</h2>
+      <Board
+        squares={squares}
+        xIsNext={xIsNext}
+        onPlay={handlePlay}
+        winner={winner}
+      />
+      <button className="restart" onClick={restartGame}>
+        Restart
+      </button>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
---
+++ tic-tac-toe/src/Components/Board.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Square from "./Square";
+import "./Board.css";
+
+function Board({ squares, xIsNext, onPlay, winner }) {
+  const handleClick = (index) => {
+    if (squares[index] || winner) {
+      return;
+    }
+    const newSquares = [...squares];
+    newSquares[index] = xIsNext ? "X" : "O";
+    onPlay(newSquares);
+  };
+
+  return (
+    <div className="board">
+      {squares.map((square, index) => {
+        return (
+          <Square
+            key={index}
+            value={square}
+            onSquareClick={() => {
+              handleClick(index);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Board;
